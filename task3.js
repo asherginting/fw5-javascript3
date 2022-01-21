e42b509336c26ecd166c8c091f17bc7820a55d2e
asherginting <asherginting3@gmail.com>
2022-01-21 08:33:30 +0700

    Program Then Catch dan Try Catch

--- task3.js
+++ task3.js
@@ -1,1 +1,49 @@
-/* Program Bebas menggunakan Promise */
+/* Bernyanyi */
+
+let janji = "bernyanyi"
+
+let p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (janji === "bernyanyi") {
+            resolve("Penonton senang")
+        }  else {
+            reject("Anda tidak bernyanyi")
+        }
+    }, 2000)
+})
+
+p.then((result) => {
+    console.log(result)
+}).catch((error) => {
+    console.log(error)
+})
+
+
+/* Beli Bakso */
+const beliBakso = (harga, duit) => {
+    return new Promise((resolve, reject)=>{
+        const change = duit - harga
+
+        if (change < 1000) {
+            reject ("Saldo tidak mencukupi")
+        } else {
+            setTimeout(()=>{
+                resolve("Pembayaran Sukses")
+            }, 2000)
+        }
+    })
+}
+
+const beli = async (harga, duit) => {
+    try{
+        console.log("Pembayaran dilakukan")
+        const result = await beliBakso(harga, duit)
+        console.log(result)
+    }catch (err){
+        console.log(err)
+    }finally{
+        console.log("Selesai")
+    }
+}
+
+beli(6000, 6500)
